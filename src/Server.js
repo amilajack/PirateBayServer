f9fc4b50d77d8cbec07e9bf9de4d85a0a6bb74b5
Amila Welihinda <amilajack@gmail.com>
2016-07-13 19:06:16 -0700

    Added search defaults

--- src/Server.js
+++ src/Server.js
@@ -4,15 +4,23 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/', (req, res, next) => {
+console.log(`Serving on localhost:${port}`);
 
+app.get('/', (req, res, next) => {
+  res.send('API')
 });
 
 app.get('/search/:searchQuery', (req, res, next) => {
   const { searchQuery } = req.params;
 
+  console.log('Searching for: ', searchQuery);
+
   PirateBay
-    .search(searchQuery)
+    .search(searchQuery, Object.assign(
+      {},
+      PirateBay.default.searchDefaults
+      req.query
+    ))
     .then(results => res.send(results));
 });
 
